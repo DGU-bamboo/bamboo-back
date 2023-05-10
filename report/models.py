2235f03fefe68a081ad0f3d42170d3fb77a5468c
0seoYun <yys020819@gmail.com>
2023-05-11 02:24:20 +0900

    feature/5: create AbstractReport model

--- report/models.py
+++ report/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from core.models import Base
+from post.models import Post
 
-# Create your models here.
+
+class AbstractReport(Base):
+    content = models.TextField()
+    password = models.CharField(max_length=15)
+    is_student = models.BooleanField(default=False)
+    is_approve = models.BooleanField(null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    filtered_content = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class NemoReport(AbstractReport):
+    pass
+
+
+class CommonReport(AbstractReport):
+    image = models.ImageField(Null=True, blank=True)
+
+
+class CommentReport(AbstractReport):
+    post_num = models.PositiveIntegerField()
+
+
+class Question(Base):
+    content = models.TextField()
+    answer = models.TextField()
