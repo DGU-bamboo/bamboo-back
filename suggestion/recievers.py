d7a84a31996f7dc73884f088fc15a02c274f0fa6
0seoYun <yys020819@gmail.com>
2023-05-15 01:34:11 +0900

    feat/9: create send_discord & suggestion receiver

---
+++ suggestion/recievers.py
@@ -0,0 +1,18 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from core.utils.discord import send_to_discord
+from suggestion.models import Suggestion
+from django.conf import settings
+
+
+@receiver(post_save, sender=Suggestion)
+def suggestion_discord_sender(sender, instance, created, **kwargs):
+    if created:
+        admin_link = (
+            f"{settings.WEB_URL}/admin/suggestion/suggestion/{instance.id}/change/"
+        )
+        url = settings.DISCORD_WEBHOOK_URL_SUGGESTION
+        message = f"""[건의사항이 도착했어요!]({admin_link})
+                    > 건의 내용 : {instance.content}
+                    > 연락처 : {instance.contact}"""
+        send_to_discord(url, message)
