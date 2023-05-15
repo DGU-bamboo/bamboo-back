be4a0ddc708e39f39760ca618065a9cbcf059dd4
dudtlstm <annpys1024@naver.com>
2023-05-15 15:50:35 +0900

    feature/8: Question API 추가

---
+++ report/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Question
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ["content", "answer"]
+
+
+class RandomQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ["id", "content"]
+
+
+class QuestionAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = "__all__"
--- report/views.py
+++ report/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework import mixins
+from .models import *
+from .serializers import *
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+
+class QuestionViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Question.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve", "update"]:
+            return QuestionAdminSerializer
+        elif self.action == "list_random_questions":
+            return RandomQuestionSerializer
+        else:
+            return QuestionSerializer
+
+    def get_permissions(self):
+        if self.action in ["create", "list", "retrieve", "update", "destroy"]:
+            return [IsAdminUser()]
+        return super().get_permissions()
+
+    @action(methods=["GET"], detail=False)
+    def list_random_questions(self, request):
+        question = Question.objects.order_by("?").first()
+        serializer = RandomQuestionSerializer(question)
+        return Response(serializer.data)
