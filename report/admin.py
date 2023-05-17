d16296309f630bf83a2625abca413603649c1c8b
tkdwns414 <sangjune2000@naver.com>
2023-05-18 00:55:26 +0900

    remove report app

--- report/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from report.models import Question
-
-from report.models import Question, CommonReport
-
-admin.site.register(Question)
-admin.site.register(CommonReport)
