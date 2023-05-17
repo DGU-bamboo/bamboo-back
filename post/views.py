b244836dc98db51b770098d6dc8c79abe464fb43
0seoYun <yys020819@gmail.com>
2023-05-18 05:29:25 +0900

    feature/34: Create Comment/PostAPIViewset

--- post/views.py
+++ post/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from requests import Response
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
+from django.db.models import Count
+from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import action
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return Post.objects.annotate(report_cnt=Count("report"))
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    @action(methods=["GET"], detail=True, permission_classes=[IsAdminUser])
+    def reject(self, request, *args, **kwargs):
+        comment = self.get_object()
+        comment.is_approved = False
+        comment.save(update_fields=["is_approved"])
+        return Response()
