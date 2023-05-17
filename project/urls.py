9869648eca65188aa554494730484b970a9777c8
0seoYun <yys020819@gmail.com>
2023-05-18 05:27:59 +0900

    feature/34 : mapping url

--- project/urls.py
+++ project/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("suggestion.urls")),
     path("api/", include("report.urls")),
+    path("api/", include("post.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
