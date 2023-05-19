1640fd482cb6e9f0e52bccbc2e7ec951039f7d49
0seoYun <yys020819@gmail.com>
2023-05-19 19:11:33 +0900

    feature/40: suggestion app admin

--- suggestion/models.py
+++ suggestion/models.py
@@ -6,3 +6,15 @@ class Suggestion(BaseModel):
     content = models.TextField(default="")
     contact = models.EmailField(blank=True, null=True)
     memo = models.TextField(blank=True, default="")
+
+
+class MaintainerSuggestion(Suggestion):
+    class Meta:
+        proxy = True
+        verbose_name = "건의사항 (관리자용)"
+        verbose_name_plural = "건의사항들 (관리자용)"
+
+    @property
+    def get_content(self, instance):
+        admin_content = instance.content[:3]
+        return admin_content
